7b968cf6eb5bc623d167b755820b4536db65fa42
aliarnoos <alimarnoos@gmail.com>
2022-12-22 16:00:57 +0300

    add cors package

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv'
 import workoutRoutes from './routes/workouts.js'
 import mongoose from 'mongoose';
+import cors from 'cors'
 dotenv.config()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
